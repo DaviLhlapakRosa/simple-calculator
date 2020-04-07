a667e9624568e6c29004ab3291fd10b43ef26a4f
DaviLhlapakRosa <alpkauniverse@gmail.com>
2020-04-07 14:44:02 -0300

    Updated function calculateResult()
    JavaScript Object Literal was used to reduce the amount of conditionals in the code.

--- js/index.js
+++ js/index.js
@@ -1,37 +1,39 @@
 
 function calculateResult() {
-    const resultTitle = document.querySelector('#result')
+    const resultTitle = document.querySelector('#result');
+
+    // Valid Operations
+    const validOperators = {
+        sum(firstValue, secondValue){
+            return firstValue + secondValue;
+        },
+        subtract(firstValue, secondValue){
+            return firstValue - secondValue;
+        },
+        multiply(firstValue, secondValue){
+            return firstValue * secondValue;
+        },
+        divide(firstValue, secondValue){
+            return firstValue / secondValue;
+        },
+    }
 
     let [firstValue, operator, secondValue] = document.querySelectorAll('.value');
-    
+
     // text to number
     firstValue = Number(firstValue.value);
-    operator = Number(operator.value);
     secondValue = Number(secondValue.value);
 
-    if (isNaN(firstValue) || isNaN(secondValue) || isNaN(operator) || operator < 0 || operator > 3) {
+    //Get the function related to the mathematical operator
+    operatorFunction = validOperators[operator.value];
+
+    if (isNaN(firstValue) || isNaN(secondValue) || operatorFunction === null) {
         resultTitle.innerHTML = 'Insira valores numéricos nos campos e/ou uma operação válida!';
         resultTitle.style.color = 'red';
         return;
     }
 
-    let result = 0;
-    switch (operator) {
-        case 0:
-            result = firstValue + secondValue;
-            break;
-        case 1:
-            result = firstValue - secondValue;
-            break;
-        case 2:
-            result = firstValue * secondValue;
-            break;
-        case 3:
-            result = firstValue / secondValue;
-            break;
-        default:
-            break;
-    }
+    let result = operatorFunction(firstValue,secondValue);
 
     resultTitle.innerHTML = result;
     resultTitle.style.color = 'black';
